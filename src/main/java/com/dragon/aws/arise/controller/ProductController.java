54e579f6c30c68ec2da793f5d9120299439f0162
Sai Kumar Dhavileswarapu <dhavileswarapusaikumar@gmail.com>
2026-03-30 22:30:39 +0530

    Added cross origin

--- src/main/java/com/dragon/aws/arise/controller/ProductController.java
+++ src/main/java/com/dragon/aws/arise/controller/ProductController.java
@@ -6,11 +6,13 @@ import com.dragon.aws.arise.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class ProductController {
     @Autowired
     ProductService productService;
